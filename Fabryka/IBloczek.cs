ecb58b9cb1542d6d6e614bd0bfa8c19c314822c7
student <asd@ww.pl>
2017-05-05 10:05:17 +0200

    bbbb

---
+++ Fabryka/IBloczek.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka.Interface
+{
+    public interface IBloczek
+    {
+        double wykonaj();
+    }
+}
