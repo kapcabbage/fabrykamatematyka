dc43c5909a203aede8ec51618f6f1f583f1156ca
student <ahmad.anadani@student.put.poznan.pl>
2017-05-05 12:03:36 +0400

    Działanie

---
+++ Fabryka/Dzialanie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka
+{
+    public class Dzialanie
+    {
+        public double zm1;
+        public double zm2;
+        public char oper;
+
+        public double wynik;
+    }
+}
--- Fabryka/Program.cs
+++ Fabryka/Program.cs
@@ -13,7 +13,7 @@ namespace Fabryka
 
 
 
-            //nic
+            
         }
     }
 }
