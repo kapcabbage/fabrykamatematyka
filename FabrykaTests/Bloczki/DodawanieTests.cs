f08556a1c89fb67f73551106ee1cf6502ca1f487
inf117319 <inf117319@LAB-43-4>
2017-05-05 09:18:41 +0200

    Addition tests

---
+++ FabrykaTests/Bloczki/DodawanieTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fabryka.Bloczki;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka.Bloczki.Tests
+{
+    [TestClass()]
+    public class DodawanieTests
+    {
+        private Dodawanie _dodawanie;
+        private Dzialanie _dzialanie;
+
+        [TestInitialize]
+        public void init()
+        {
+            _dzialanie = new Dzialanie();
+            _dzialanie.oper = '+';
+            _dodawanie = new Dodawanie();
+        }
+
+        [TestMethod()]
+        public void wykonajUjemnaTest()
+        {
+            _dzialanie.zm1 = 1;
+            _dzialanie.zm2 = -2;
+            _dodawanie.wykonaj(_dzialanie);
+            Assert.AreEqual(_dzialanie.wynik, -1);
+
+        }
+        [TestMethod()]
+        public void wykonajZwykłyTest()
+        {
+            _dzialanie.zm1 = 1;
+            _dzialanie.zm2 = 3;
+            _dodawanie.wykonaj(_dzialanie);
+            Assert.AreEqual(_dzialanie.wynik, 4);
+        }
+    }
+}
