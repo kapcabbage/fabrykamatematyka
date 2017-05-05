d6050c0751e025f164358f0653971249d8dc766f
student <asd@ww.pl>
2017-05-05 10:44:26 +0200

    customowy

---
+++ Fabryka/Bloczki/Customowy.cs
@@ -0,0 +1,26 @@
+﻿using Fabryka.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabryka.Bloczki
+{
+    public class Customowy : IBloczek
+    {
+        private char Operator = '(';
+
+        public void wykonaj(Dzialanie dzialanie)
+        {
+            if (dzialanie.wynik == null)
+            {
+
+                if (dzialanie.oper == Operator)
+                {
+                    dzialanie.wynik = (dzialanie.zm1 + dzialanie.zm2) * 2;
+                }
+            }
+        }
+    }
+}
